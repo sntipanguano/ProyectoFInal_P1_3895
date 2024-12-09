4cb12bf9e3ffa35acd66aea24696ac0e102ebf6a
Luciana Noemi Guerra López <75289552+10038-lnguerra@users.noreply.github.com>
2024-12-09 16:47:00 -0500

    Atc

--- Back/Rest/Rest4/src/main/java/taller1/taller1/Repositorio/UsuariosRepository.java
+++
@@ -1,7 +0,0 @@
-package taller1.taller1.Repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import taller1.taller1.Modelo.Usuarios;
-
-public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
-}
--- Back/Soap/src/main/java/com/soap/soap/repository/LibrosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.soap.soap.repository;
-
-import com.soap.soap.model.LibrosModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibrosRepository extends JpaRepository<LibrosModel, Integer> {
-}
